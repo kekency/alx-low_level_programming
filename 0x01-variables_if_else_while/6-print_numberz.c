424350bb6d121ec111a86d120a79d74202e0da3e
Anibe Abah <anibeabah@gmail.com>
2023-09-09 10:48:23 -0700

    prints numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers without the use of char
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
