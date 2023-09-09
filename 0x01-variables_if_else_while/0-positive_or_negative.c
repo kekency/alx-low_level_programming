8a29cebb706085a6a3fd3dd148a21f5a473a163d
Anibe Abah <anibeabah@gmail.com>
2023-09-09 07:48:26 -0700

    Created shorter characters to match expected output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("The number %d generated is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("The number %d generated is Zero\n", n);
+		printf("%d is Zero\n", n);
 	}
 	else
 	{
-		printf("The number %d generated is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	
 	return(0);
