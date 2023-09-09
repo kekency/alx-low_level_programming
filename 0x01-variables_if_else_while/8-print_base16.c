b21cf8c3933b747a3e91c261cdebc81b32f7ae59
Anibe Abah <anibeabah@gmail.com>
2023-09-09 14:47:55 -0700

    coding style

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+
 /**
- * main - Prints numbers in base16
- *
- * return - Always (Success)
- */
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = '0'; c <= '9'; c++)
-		{
-			putchar(c);
-}
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
 	for (c = 'a'; c <= 'f'; c++)
-		{
-			putchar(c);}
+	{
+		putchar(c);
+	}
+
 	putchar('\n');
 
 	return (0);
 }
-                
