30e0c7a0a73e2195dd285b74434efc39cf3f42f8
Anibe Abah <anibeabah@gmail.com>
2023-09-09 08:28:44 -0700

    Alpha bets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+{
+	char k;	
+	for (k = 'a'; k <= 'z'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
