65c408a5797f7c00f3991ee52b8eee6f90b78715
Anibe Abah <anibeabah@gmail.com>
2023-09-12 16:36:46 -0700

    fibunacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Initializes the first two numbers
+ *
+ * Return: Always (zero)
+ */
+int main(void)
+{
+int fibonacci1 = 1;
+int fibonacci2 = 2;
+
+printf("%d, %d, ", fibonacci1, fibonacci2);
+
+int count = 2;
+
+while (count < 98)
+{
+int fibonacci_next = fibonacci1 + fibonacci2;
+
+printf("%d, ", fibonacci_next);
+
+fibonacci1 = fibonacci2;
+fibonacci2 = fibonacci_next;
+
+count++;
+}
+
+printf("\n");
+
+return (0);
+}
