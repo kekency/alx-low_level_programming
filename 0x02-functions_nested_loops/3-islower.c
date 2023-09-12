1bcc3a287c9c91e453f5f03ba7fc9089cb530877
Anibe Abah <anibeabah@gmail.com>
2023-09-12 14:05:35 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - prints all lowercase characters
+ *
+ * return: 1 if c is lower, otherwise 0
+ */
+int _islower(int c)
+{
+	for (c = 97; c <= 122; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
