535458ffbce47173ec07e81107478ddbc6a63c8d
Anibe Abah <anibeabah@gmail.com>
2023-09-21 11:32:26 -0700

    betty update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,19 @@ char *cap_string(char *s)
 {
 	int a = 0, i;
 	int cspch = 13;
-	char spch[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
 	while (s[a])
 	{
 		i = 0;
 
 		while (i < cspch)
 		{
-			if ((a == 0 || s[a - 1] == spch[i]) && (s[a] >= 97 && s[a] <= 122))
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
 				s[a] -= 32;
 			i++;
 		}
 		a++;
 	}
-	return(s);
+	return (s);
 }
