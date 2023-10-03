46743c51a1338aa2ef9201486b5ffd54541837ac
Anibe Abah <anibeabah@gmail.com>
2023-10-03 09:11:31 -0700

    argc ctd

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,11 +8,9 @@
  *
  * Return: Always Success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void) argv;
-
-	printf("%d \n", argc - 1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
